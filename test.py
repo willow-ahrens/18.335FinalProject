376b529d49e1c1daf27032993ed5af5bb38e8a71
Willow Ahrens <willow.marie.ahrens@gmail.com>
2017-05-17 16:36:48 -0400

    this is not an empty commit

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import numpy as np
 from utils import *
 from TFGDCANDECOMP import *
-from NPGDCANDECOMP import *
+from FOOCANDECOMP import *
 
 #boring accuracy test examples
 X = NormalTensorComposition((10, 10, 10), 5)
@@ -14,17 +14,33 @@ A = np.array([[1.0, 0.0], [2.0, 1.0], [1.0, 2.0]]).T
 B = np.array([[0.0, 1.0], [2.0, 2.0], [1.0, 2.0]]).T
 C = np.array([[1.0, 0.0], [0.0, 1.0], [2.0, 1.0]]).T
 X = np.einsum('ir,jr,kr->ijk', A, B, C)
-(s, A_out, B_out, C_out, error) = NPGDCANDECOMP(X, 4)
-Y = np.einsum('r,ir,jr,kr->ijk', s, A_out, B_out, C_out)
-error = accuracy(X, 3, s, A_out, B_out, C_out)
+(s_out, A_out, B_out, C_out, error) = NPGDCANDECOMP(X, 3)
+Y = np.einsum('r,ir,jr,kr->ijk', s_out, A_out, B_out, C_out)
+error = accuracy(X, 3, s_out, A_out, B_out, C_out)
 print("error: ", error)
+
 print("s")
-print(s)
+a_nrm = np.linalg.norm(A, ord = 2, axis = 0)
+A /= a_nrm
+b_nrm = np.linalg.norm(B, ord = 2, axis = 0)
+B /= b_nrm
+c_nrm = np.linalg.norm(C, ord = 2, axis = 0)
+C /= c_nrm
+print(a_nrm*b_nrm*c_nrm)
+print("s_out")
+print(s_out)
+
 print("A")
+print(A)
+print("A_out")
 print(A_out)
 print("B")
+print(B)
+print("B_out")
 print(B_out)
 print("C")
+print(C)
+print("C_out")
 print(C_out)
 print("X")
 print(X)
